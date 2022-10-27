dd196d7c3d2ffe3e626c368a1cc07bda803752a9
Joao Pitoli <joao.contats@gmail.com>
2022-10-27 14:48:12 -0300

    Criação da Classe SmartTv e Implem. de Métodos

---
+++ sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,45 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 1;
+
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+        System.out.println("Canal Atual Digitado: " + novoCanal);
+    }
+
+    public void aumentarCanal(){
+        canal++; 
+        System.out.println("Canal Atual:" + canal);
+    }
+
+    public void diminuirCanal(){
+        canal--;  
+        System.out.println("Canal Atual:" + canal);
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando o volume para: " + volume);
+    }
+
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo o volume para: " + volume);
+    }
+
+
+    public void ligar(){
+        ligada = true;
+        System.out.println("SmartTv Ligada!");
+
+    }
+
+    public void desligar(){
+        ligada = false;
+        System.out.println("SmartTv Desligada!");
+    }
+
+
+}
---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,22 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.ligar();
+        smartTv.mudarCanal(16);
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarVolume();
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.mudarCanal(9);
+        smartTv.aumentarVolume();
+        smartTv.desligar();
+
+
+        
+
+
+    }
+}
